e1ed0feb4e7d0048ff599d87b5636dcff6122ebc
Vinicius Bernardes Silva <vinicius.bsilva@hotmail.com>
2022-11-11 23:33:42 -0300

    Lessons 1 to 6

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+const About = () => {
+    return ( 
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae doloremque nostrum odio, sapiente cum impedit quibusdam porro dignissimos provident minus tempore quo, delectus, non nihil rem soluta veniam blanditiis dolorum.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae doloremque nostrum odio, sapiente cum impedit quibusdam porro dignissimos provident minus tempore quo, delectus, non nihil rem soluta veniam blanditiis dolorum.</p>
+        </div>
+     );
+}
+ 
+export default About;
---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from "next/head"
+import Link from "next/link"
+import styles from "../styles/Home.module.css"
+
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className={styles.title}>Homepage</h1>
+      <p className={styles.text}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae doloremque nostrum odio, sapiente cum impedit quibusdam porro dignissimos provident minus tempore quo, delectus, non nihil rem soluta veniam blanditiis dolorum.</p>
+      <p className={styles.text}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae doloremque nostrum odio, sapiente cum impedit quibusdam porro dignissimos provident minus tempore quo, delectus, non nihil rem soluta veniam blanditiis dolorum.</p>
+      <Link href="/ninjas" className={styles.btn}>See Ninjas Listing</Link>
+    </div>
+  )
+}
